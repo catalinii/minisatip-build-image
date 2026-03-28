6adcf76727ebfbec7fdc0f36c75dad0fe852ce82
Catalin Toda <catalinii@gmail.com>
2026-03-28 20:00:55 +0000

    fix mips

--- build_libraries.sh
+++ build_libraries.sh
@@ -1,31 +1,51 @@
-#! /bin/bash -e
-DIR=/tmp/
-BASEDIR=$(dirname "$0")
+#! /bin/bash -ex
+BUILD_DIR=/tmp
+BASEDIR="$(cd "$(dirname "$0")" && pwd)"
 
-cd $DIR
+# Parse --build-dir argument if provided
+for arg in "$@"; do
+  case "$arg" in
+    --build-dir=*)
+      BUILD_DIR="${arg#--build-dir=}"
+      ;;
+    --build-dir)
+      BUILD_DIR_SET=true
+      ;;
+  esac
+done
+
+# Handle --build-dir with separate value
+if [[ "${BUILD_DIR_SET:-}" == "true" ]]; then
+  shift
+  BUILD_DIR="$1"
+  shift
+fi
+
+export BUILD_DIR
+cd $BUILD_DIR
 rm -rf srt libdvbcsa libnetceiver openssl
-mkdir -p $DIR/openssl
+mkdir -p $BUILD_DIR/openssl
 git clone https://github.com/catalinii/libdvbcsa
-curl -L -s https://github.com/openssl/openssl/releases/download/openssl-3.5.4/openssl-3.5.4.tar.gz | tar xzf - -C /tmp/openssl --strip-components=1
+curl -L -s https://github.com/openssl/openssl/releases/download/openssl-3.5.4/openssl-3.5.4.tar.gz | tar xzf - -C $BUILD_DIR/openssl --strip-components=1
 git clone https://github.com/vdr-projects/libnetceiver/
 git clone https://github.com/Haivision/srt
 
 # Build libdvbcsa
 $BASEDIR/build_libdvbcsa.sh 2>&1 | tail -200
 $BASEDIR/build_libdvbcsa.sh --host=arm-linux-gnueabihf --prefix=/usr/arm-linux-gnueabihf/ 2>&1 | tail -200
-$BASEDIR/build_libdvbcsa.sh --host=mipsel-linux-gnu --prefix=/opt/mipsel 2>&1 | tail -200
+$BASEDIR/build_libdvbcsa.sh --host=mipsel-linux-gnu --prefix=/opt/mipsel/usr 2>&1 | tail -200
 
 # Build openssl
 $BASEDIR/build_openssl.sh linux-generic64 2>&1 | tail -200
 $BASEDIR/build_openssl.sh --cross-compile-prefix=arm-linux-gnueabihf- --prefix=/usr/arm-linux-gnueabihf/ linux-generic32 2>&1 | tail -200
-$BASEDIR/build_openssl.sh --cross-compile-prefix=mipsel-linux-gnu- --prefix=/opt/mipsel linux-generic32 2>&1 | tail -200
+$BASEDIR/build_openssl.sh --cross-compile-prefix=mipsel-linux-gnu- --prefix=/opt/mipsel/usr linux-generic32
 
 # Build netceiver (x64 only)
-$BASEDIR/build_netceiver.sh 2>&1 | tail -200
+$BASEDIR/build_netceiver.sh
 
 # Build srt
 $BASEDIR/build_srt.sh
 CC=arm-linux-gnueabihf-gcc CXX=arm-linux-gnueabihf-g++ CMAKE_INSTALL_PREFIX=/usr/arm-linux-gnueabihf/ $BASEDIR/build_srt.sh
-CC=mipsel-linux-gnu-gcc CXX=mipsel-linux-gnu-g++ CMAKE_INSTALL_PREFIX=/opt/mipsel $BASEDIR/build_srt.sh
+CC=mipsel-linux-gnu-gcc CXX=mipsel-linux-gnu-g++ CMAKE_INSTALL_PREFIX=/opt/mipsel/usr $BASEDIR/build_srt.sh
 
 ldconfig
--- build_openssl.sh
+++ build_openssl.sh
@@ -2,9 +2,27 @@
 
 set -euo pipefail
 
-DIR=/tmp/
+DIR=${BUILD_DIR:-/tmp}/
 
 cd $DIR/openssl
+
+# Extract PREFIX from arguments for cross-compiler PATH setup
+PREFIX=""
+for arg in "$@"; do
+  case "$arg" in
+    --prefix=*)
+      PREFIX="${arg#--prefix=}"
+      ;;
+  esac
+done
+
+# If cross-compiling with a custom prefix, add the bin directory to PATH
+if [ -n "$PREFIX" ] && [ "$PREFIX" != "/usr/local" ]; then
+  if [ -d "$PREFIX/bin" ]; then
+    export PATH="$PREFIX/bin:$PATH"
+  fi
+fi
+
 ./Configure $@
 if [ $(echo $@| grep mips | wc -l | awk '{print $1;}') -gt 0 ]; then
 	make EX_LIBS=-latomic  -j $(nproc)
